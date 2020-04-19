918ac4818bb792d5790f1e88f387331243691e2c
Mark Van de Vyver <mark@taqtiqa.com>
2020-04-19 19:15:01 +1000

    Fix env script
    Signed-off-by: Taqtiqa LLC <admin@taqtiqa.com>

--- scripts/ocix-env.sh
+++ scripts/ocix-env.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-echo "export OCIX_REGISTRY=$(cat ocix_registry)"
-echo "export OCIX_REGISTRY=$(cat ocix_port)"
-echo "export OCIX_ORG=$(cat ocix_org)"
-echo "export OCIX_VERSION=$(cat ocix_version)"
+echo 'export OCIX_REGISTRY="$(cat ocix_registry)"'
+echo 'export OCIX_PORT="$(cat ocix_port)"'
+echo 'export OCIX_ORG="$(cat ocix_org)"'
+echo 'export OCIX_VERSION="$(cat ocix_version)"'
